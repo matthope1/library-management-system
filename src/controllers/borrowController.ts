8668f3c6f90aee4e30c842c75a41f0a2ee234b16
Matt Hope <matt-hope@hotmail.com>
2024-03-06 15:40:35 -0500

    fixed tx update issue with return book & updated borrow book

--- src/controllers/borrowController.ts
+++ src/controllers/borrowController.ts
@@ -3,56 +3,80 @@ import Transaction from '../transaction'
 import { log } from 'console'
 
 
-export const borrowBook = (req: Request, res: Response, next: NextFunction): void => { 
+export const borrowBook = async (req: Request, res: Response, next: NextFunction) => { 
     try {
-        log('borrow book endpoint')
         const { bookID, userID, borrowDate } = req.body
-        log("req.body", req.body)
         if (!bookID || !userID || !borrowDate ) {
-            throw new Error('missing bookID/userID/borrowDate')
+            const errMessage = 'missing bookID/userID/borrowDate'
+            next(errMessage)
         }
 
-        let dateTime = new Date()
-        log('datetime', dateTime)
+        // if user already borrowed this book, check if they have returned it
+        // if not then don't allow borrow call
+        // if they have returned it then remove return date and update borrow date
 
-        const transaction = new Transaction({
-            bookID,
-            userID,
-            // borrowDate
-            dateTime
-        })
+        const filter = {'bookID': bookID, 'userID': userID}
+        const found = await Transaction.findOne(filter).exec()
 
-        transaction.save() 
+        log('found', found)
+
+        if (!found) {
+            // if no tx for this book/user exists 
+            log('not found')
+            const transaction = new Transaction({
+                bookID,
+                userID,
+                borrowDate,
+            })
+
+            transaction.save() 
+        }
+
+        if (found && !found.returnDate) {
+            log('found and no return date')
+            const errMessage = 'User already borrowed book' 
+            next(errMessage)
+        }
+
+        if (found && found.returnDate) {
+            log('found and return date ')
+            const update = {
+                returnDate: "",
+                borrowDate
+            }
+            const updated = await Transaction.findOneAndUpdate(filter, update, {new: true})
+            if (!updated) {
+                next('Failed to update transaction')
+            }
+        } 
 
         res.json({message: 'borrowed book successfully'})
+   
     } catch(err) {
         log('err', err)
         next(err)
     }
 }
 
-export const returnBook = (req: Request, res: Response, next: NextFunction): void => { 
+export const returnBook = async (req: Request, res: Response, next: NextFunction) => { 
     try {
-        log('return book endpoint')
         const { bookID, userID, returnDate } = req.body
-        // get transaction based on book id & userid? 
         const filter = {'bookID': bookID, 'userID': userID}
-        const found = Transaction.find(filter)
-
-        let dateTime = new Date()
+        const found = await Transaction.findOne(filter).exec()
 
         if (!found) {
-            throw new Error('transaction not found')
-        }
-        // update the return date
-        const update = {
-            returnDate: dateTime,
-            // returnDate: returnDate
+            throw new Error('Transaction not found');
         }
 
-        // TODO fix me this doesn't update
-        let doc = Transaction.findOneAndUpdate(filter,update)
-        
+        const update = {
+            returnDate
+        }
+
+        const updatedTransaction = await Transaction.findOneAndUpdate(filter, update, { new: true });
+
+        if (!updatedTransaction) {
+            throw new Error('Failed to update transaction');
+        }
 
         res.json({message: 'returned book successfully'})
     } catch(err) {
