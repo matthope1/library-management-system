94d3cef220a8a86078e89952691b6c0f5506c98f
Matt Hope <matt-hope@hotmail.com>
2024-03-08 23:55:32 -0500

    remove extra code

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -93,6 +93,4 @@ form.addEventListener("submit", (e) => {
     e.preventDefault();
     const formData = new FormData(e.target);
     const formProps = Object.fromEntries(formData); console.log("form props", formProps)
-
-
 });
